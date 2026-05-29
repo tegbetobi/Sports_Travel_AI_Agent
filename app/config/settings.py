80f9af5447ea1d25ed0d84eb40f95dc840f1c413
Tobi <99181684+tegbetobi@users.noreply.github.com>
2026-05-29 05:45:11 +0200

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -7,8 +7,6 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
-AMADEUS_CLIENT_ID = os.getenv("AMADEUS_CLIENT_ID")
-AMADEUS_CLIENT_SECRET = os.getenv("AMADEUS_CLIENT_SECRET")
 
 
 
